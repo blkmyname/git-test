51ca521a154812a9ce825334d28ca984fea4c4f7
blkmyname <13894339755@163.com>
2023-10-18 20:54:36 +0800

    推送远程仓库

---
+++ study/helloworld-app/src/com/blkmyname/speedkey/speedkeyDemo.java
@@ -0,0 +1,9 @@
+package com.blkmyname.speedkey;
+
+public class speedkeyDemo {
+    public static void main(String[] args) {
+//        System.out.println("hello world2");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
